726b380c10fc3e8b24c3bc8e79c172fb82ececc8
LizzieGuajardo <32180546+LizzieGuajardo@users.noreply.github.com>
2019-05-12 23:20:24 -0500

    css

--- FrontEndTest/js/receta.js
+++ FrontEndTest/js/receta.js
@@ -45,6 +45,15 @@ function loadRecipe() {
 		$("#difficulty").addClass(color)
 		$("#porciones").html(" "+data.servings)
 
+		 var clasifTxt = "";
+		 data.clasif.forEach(function(name)
+		 {
+		 	clasifTxt += " "+name+" /";
+		 })
+		 clasifTxt = clasifTxt.substring(0,clasifTxt.length-1);
+		 $("#clasificaciones").html(clasifTxt);
+
+
 		for (var i = data.ingredients.length - 1; i >= 0; i--) {
 			$("#ingredients").append(`<li>${data.ingredients[i]}</li>`)
 		}
@@ -52,6 +61,12 @@ function loadRecipe() {
 		for (var i = data.steps.length - 1; i >= 0; i--) {
 			$("#steps").append(`<li>${data.steps[data.steps.length -i -1]}</li>`)
 		}
+
+		 $("#calorias").html(data.nutrition.kcal);
+		 $("#grasas").html(data.nutrition.fat + " g");
+		 $("#saturadas").html(data.nutrition.saturates + " g");
+		 $("#azucar").html(data.nutrition.sugar + " g");
+		 $("#sal").html(data.nutrition.salt + " g");
     },
     error: function(error_msg) {
       alert((error_msg['responseText']));
